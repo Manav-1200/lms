f7572383fe74e5a995200a848c17675eb45bc638
Manav-1200 <manav.neupane1@gmail.com>
2025-10-29 15:22:37 +0545

    fixed CRUD error

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,13 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-
-def welcome(request):
-    
-    return render(request, "accounts/welcome.html")
+from django.shortcuts import render
 
 @login_required
 def profile(request):
-
-    return render(request, "accounts/profile.html")
-
-def logout_view(request):
-    
-    logout(request)
-    return redirect("welcome")
+    user = request.user
+    context = {
+        "user": user,
+        "is_admin": user.role == "admin",
+        "is_instructor": user.role == "instructor",
+        "is_student": user.role == "student",
+    }
+    return render(request, "accounts/profile.html", context)
